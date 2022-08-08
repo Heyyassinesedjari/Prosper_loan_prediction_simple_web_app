dbb730d440ae0026c6561e4496a095ac6d14b94f
Yassine SEDJARI <94799575+Heyyassinesedjari@users.noreply.github.com>
2022-08-08 15:55:15 +0100

    Delete deployment directory

--- deployment/application/model.py
+++
@@ -1,8 +0,0 @@
-import pickle 
-
-modelfilename = 'application/Logistic_Regression.pkl'
-scalerfilename = 'application/Scaler.pkl'
-
-model = pickle.load(open(modelfilename, 'rb'))
-
-scaler = pickle.load(open(scalerfilename, 'rb'))
